ab5bdd7d26a92a70d6126d1d74634c2c77bf9465
PastelPossum <iggyskywalker@gmail.com>
2023-02-01 11:02:41 +0100

    Add quotes in array

---
+++ script.js
@@ -0,0 +1,11 @@
+let quotes = ["Du må selv være den forandring, som du ønsker at se i verden _ Mahatma Gandhi",
+"Livet må leves forfra, men kan kun forstås bagfra _ Søren Kierkegaard",
+"Det er tungt, forbandet tungt, at dø mens hjertet stadig er ungt _ H. C. Andersen",
+"Vi er alle i rendestenen, men nogle af os kigger mod stjernerne _ Oscar Wilde",
+"Når en blind leder en blind, falder de begge i graven _ Bibelen",
+"Det, der ikke dræber os gør os stærkere _ Friedrich Nietzsche",
+"Folk er ligeså lykkelige, som de beslutter sig for at være _ Abraham Lincoln",
+"Du skal ikke græde over noget er forbi, men glæde dig over det er sket _ Dr. Seuss",
+"Det store er ikke at være enten det ene eller det andet, men at være sig selv _ og det formår hvert eneste menneske, hvis det vil — Søren Kierkegaard",
+"Latter er den korteste afstand mellem to mennesker _ Victor Borge"
+]
